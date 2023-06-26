88c02c9aa0a7d167bc013db583418942ef0511c0
Evelyn Elena Campos <elenuco36@gmail.com>
2023-06-26 11:30:53 -0600

    feat
    New documents

---
+++ HousesSytem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HousesSytem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
