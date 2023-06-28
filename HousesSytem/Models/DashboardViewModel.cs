d5c838960cdcce535eeb165b3fc0e5b20c4c830b
Evelyn Elena Campos <elenuco36@gmail.com>
2023-06-28 11:04:57 -0600

    Anexo de model base
    se anexo el modelo base

---
+++ HousesSytem/Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HousesSytem.Models
+{
+    public class DashboardViewModel
+    {
+        public int Id { get; set; }
+        public int TotalHabitaciones { get; set; }
+        public int HabitacionesDisponibles { get; set; }
+        public int HabitacionesOcupadas { get; set; }
+    }
+}
